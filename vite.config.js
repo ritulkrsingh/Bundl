bce9985db9f3b44294abfff74701d6679592cfde
ritul_kr_singh <ritullefo1111@gmail.com>
2024-07-02 01:08:40 +0530

    basic prototype

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const mode = process.env.MODE || 'development'
+const mode = process.env.NODE_ENV || 'development'
 const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
 const env = loadEnv(mode, process.cwd(), '')
