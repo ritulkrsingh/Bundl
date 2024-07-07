97afd839be16ce1d7763e8f4cdfc7aebfb307bcc
ritul_kr_singh <ritullefo1111@gmail.com>
2024-07-07 08:17:59 +0530

    Option to delete account

--- src/parts/Home.jsx
+++ src/parts/Home.jsx
@@ -25,7 +25,6 @@ const RestaurantList = () => {
     const fetchRestaurants = async () => {
       const response = await axios.get(url + 'api/restaurants');
       setRestaurants(response.data ? response.data : []);
-      console.log(url + "api/restaurants got", response.data);
     };
 
     fetchRestaurants();
